87d7e7773a6df7f7ef989ddf912bd9f6f73d544b
Luke Schaefer <luke@schaefer.email>
2025-01-27 16:08:50 -0700

    Added ability to use FontAwesome class names in Fluent Icon fields.

---
+++ YOCA.Fluent/Models/CustomIcons.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace YOCA.Fluent.Models;
+
+public class CustomIcons
+{
+
+}
+
+public static class FontAwesome
+{
+    public static Icon Icon(string iconName) => new Icon(iconName, IconVariant.Regular, IconSize.Size16, $"<span class=\"{iconName}\"></span>");
+}
